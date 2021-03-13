6be8fbf3b6037b1494dd7ef0609c25461dbcbcc2
Stephen Choate <schoate@me.com>
2021-03-12 21:37:20 -0500

    Initial Commit

---
+++ SimpleSlots/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SimpleSlots
+//
+//  Created by Stephen Choate on 3/12/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
